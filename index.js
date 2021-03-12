67853978fae8b027e5bc7ca0ad3086d7eb0c8a2f
devsarg <80121904+devsarg@users.noreply.github.com>
2021-03-12 11:29:27 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -31,8 +31,79 @@ app.get("/data", (req, res) => {
     res.render("data", { model: test });
   });
 
+  app.get("/books", (req, res) => {
+    const sql = "SELECT * FROM Books ORDER BY Title"
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      res.render("books", { model: rows });
+    });
+  });
+
+app.get("/edit/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM Books WHERE Book_ID = ?";
+    db.get(sql, id, (err, row) => {
+      // if (err) ...
+      res.render("edit", { model: row });
+    });
+}); 
+
+// POST /edit/5
+app.post("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const book = [req.body.Title, req.body.Author, req.body.Comments, id];
+  const sql = "UPDATE Books SET Title = ?, Author = ?, Comments = ? WHERE (Book_ID = ?)";
+  db.run(sql, book, err => {
+    // if (err) ...
+    res.redirect("/books");
+  });
+});
+
+app.get("/create", (req, res) => {
+  res.render("create", { model: {} });
+});
+
+// GET /create
+app.get("/create", (req, res) => {
+  const book = {
+    Author: "Victor Hugo"
+  }
+  res.render("create", { model: book });
+});
+
+// POST /create
+app.post("/create", (req, res) => {
+  const sql = "INSERT INTO Books (Title, Author, Comments) VALUES (?, ?, ?)";
+  const book = [req.body.Title, req.body.Author, req.body.Comments];
+  db.run(sql, book, err => {
+    // if (err) ...
+    res.redirect("/books");
+  });
+});
+
+// GET /delete/5
+app.get("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM Books WHERE Book_ID = ?";
+  db.get(sql, id, (err, row) => {
+    // if (err) ...
+    res.render("delete", { model: row });
+  });
+});
+
+// POST /delete/5
+app.post("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "DELETE FROM Books WHERE Book_ID = ?";
+  db.run(sql, id, err => {
+    // if (err) ...
+    res.redirect("/books");
+  });
+});
+
 app.set("view engine", "ejs");
-
-app.set("views", __dirname + "/views");
-
-app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }))
